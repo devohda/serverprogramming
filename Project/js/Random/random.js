a382534c133e1218161cfec8890d84b47d6b6e42
SEOKJUN-KO <tjrwns1021@gmail.com>
2020-07-05 00:04:49 +0900

    랜덤페이지 ajax 00.01 2020.07.05

---
+++ Project/js/Random/random.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    // 지역 + 카테고리 클릭시 함수
+    var i = 0;
+    $('.food').click(function () {
+        i = i + 1;
+        $(document)
+            .find('.course-area')
+            .append(
+                '<div class="randomfood"><img class="resultimg' +
+                    i +
+                    '" src="" /><div id="course"><p id="course' +
+                    i +
+                    '">코스 ' +
+                    i +
+                    '</p><p id="resulttitle' +
+                    i +
+                    '"></p><a href="" id="resulthref' +
+                    i +
+                    '><p id="resultlink' +
+                    i +
+                    '"></p></a></div><div class="button-area"><form method="GET" name="result" id="result"><input type="button" name="" id="rerandom" value="다시뽑기" /></form></div></div>',
+            );
+    });
+});
+//<![CDATA[
+$(function () {
+    var i = 0;
+    $('.food').click(function () {
+        i = i + 1;
+        $.ajax({
+            url: 'http://59.6.42.102:8080/api/random/test', //데이터전송및요청할URL 주소
+            type: 'GET',
+            dataType: 'JSON',
+
+            error: function () {
+                alert('통신실패!!!!');
+            },
+            success: function (result) {
+                $(document)
+                    .find('.resultimg' + i)
+                    .attr('src', result.data[0].img);
+                $(document)
+                    .find('#resulttitle' + i)
+                    .append(result.data[0].title);
+                $(document)
+                    .find('#resulthref' + i)
+                    .attr('href', result.data[0].link);
+                $(document)
+                    .find('#resultlink' + i)
+                    .append('링크');
+            },
+        });
+
+        return false; //action 페이지로전환되는것을차단합니다.
+    });
+});
+//]]>
---
+++ Project/js/Random/random_rest.js
@@ -0,0 +1,22 @@
+// 20200704100006
+// http://59.6.42.102:8080/api/random/pick?keyword=%EC%84%B8%EC%A2%85%EB%8C%80%20%EB%A7%9B%EC%A7%91
+// 세종대 맛집 랜덤 검색 결과
+const response = {
+    "result": "ok",
+    "data": [
+      {
+        "title": "김부삼 건대점",
+        "content": "",
+        "category": [
+          "돼지고기구이"
+        ],
+        "address": "",
+        "tags": [
+          
+        ],
+        "img": "https://search.pstatic.net/common/?autoRotate=true&quality=95&src=http%3A%2F%2Fldb.phinf.naver.net%2F20160713_184%2F1468348469817LhMch_JPEG%2F176663436449695_0.jpeg&type=f150_150",
+        "link": "https://store.naver.com/restaurants/detail?entry=ple&id=38248686&query=%EA%B9%80%EB%B6%80%EC%82%BC%20%EA%B1%B4%EB%8C%80%EC%A0%90",
+        "review": "327"
+      }
+    ]
+  }
