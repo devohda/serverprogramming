79aa3e6ea46b8b25a765109691bae8a42fdece8c
devohda <fffvvv6@gmail.com>
2020-07-05 22:36:11 +0900

    반응형 구현

--- Project/js/Random/random.js
+++ Project/js/Random/random.js
@@ -1,34 +1,27 @@
 i = 0;
 $(document).ready(function () {
     // 지역 + 카테고리 클릭시 함수
-    $('.food').click(function () {
+    $('.food').click(function food() {
         i = i + 1;
         $(document)
             .find('.course-area')
             .append(
                 '<div class="course-result">' +
                     '<img class="resultimg' +
-                    i +
                     '" src="" />' +
                     '<div class="course">' +
                     '<p class="resulttitle' +
-                    i +
                     '"></p>' +
                     '<a href"" class="resulthref' +
-                    i +
                     '">' +
                     '<p class="resultlink' +
-                    i +
                     '">링크</p>' +
                     '</a>' +
                     '<p class="resultaddress' +
-                    i +
                     '">1&nbsp;</p>' +
                     '<p class="resultcategory' +
-                    i +
                     '">2&nbsp;</p>' +
                     '<p class="resulttags' +
-                    i +
                     '">3&nbsp;</p>' +
                     '</div>' +
                     '<div class="erase">삭제</div>' +
@@ -39,6 +32,7 @@ $(document).ready(function () {
                     '</div>' +
                     '</div>',
             );
+            return 
     });
     $('.cafe').click(function () {
         i = i + 1;
@@ -170,28 +164,20 @@ $(function () {
                 alert('통신실패!!!!');
             },
             success: function (result) {
-                $(document)
-                    .find('.resultimg' + i)
-                    .attr('src', result.data[0].img);
-                $(document)
-                    .find('.resulttitle' + i)
-                    .append(result.data[0].title);
-                $(document)
-                    .find('.resulthref' + i)
-                    .attr('href', result.data[0].link);
-                $(document)
-                    .find('.resultaddress' + i)
-                    .append(result.data[0].address);
+                $(document).find('.resultimg').attr('src', result.data[0].img);
+                $(document).find('.resulttitle').append(result.data[0].title);
+                $(document).find('.resulthref').attr('href', result.data[0].link);
+                $(document).find('.resultaddress').append(result.data[0].address);
                 var s = result.data[0].category;
                 $(s).each(function (index, item) {
                     $(document)
-                        .find('.resultcategory' + i)
+                        .find('.resultcategory')
                         .append(item + ' ');
                 });
                 var s2 = result.data[0].tags;
                 $(s2).each(function (index, item) {
                     $(document)
-                        .find('.resulttags' + i)
+                        .find('.resulttags')
                         .append('"' + item + '" ');
                 });
             },
@@ -204,4 +190,21 @@ $(function () {
 $(document).on('click', '.erase', function () {
     $(this).parent().remove();
     i = i - 1;
+});
+
+$(document).on('click', '.rerandom', function () {
+    var box = $(this).parents('.course-result');
+    $.ajax({
+        url: 'http://59.6.42.102:8080/api/random/test', //데이터전송및요청할URL 주소
+        type: 'GET',
+        dataType: 'JSON',
+
+        error: function () {
+            alert('통신실패!!!!');
+        },
+        success: function (result) {
+            console.log(result);
+            box.find('.resultimg').attr('src', result.data[0].img);
+        },
+    });
 });
