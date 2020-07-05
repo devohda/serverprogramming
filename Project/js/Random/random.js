1f35c26eba74ccd8a957297cc04a5070d1fe7c0f
devohda <fffvvv6@gmail.com>
2020-07-06 00:54:54 +0900

    div 생성

--- Project/js/Random/random.js
+++ Project/js/Random/random.js
@@ -1,38 +1,74 @@
 i = 0;
 $(document).ready(function () {
     // 지역 + 카테고리 클릭시 함수
+
+    //음식점 카페 주점 놀기 중 하나 선택시 div 만들기.
+    const COURSE = document.querySelector('.course-area');
+
+    function makingCourse() {
+        const course_result = document.createElement('div');
+        course_result.classList.add('course-result');
+
+        const result_img = document.createElement('img');
+        result_img.classList.add('result-img');
+
+        const course = document.createElement('div');
+        course.classList.add('course');
+
+        const result_title = document.createElement('p');
+        result_title.classList.add('result-title');
+        const result_href = document.createElement('a');
+        result_href.classList.add('result-href');
+
+        const result_content = document.createElement('div');
+        result_content.classList.add('result-content');
+
+        const result_address = document.createElement('p');
+        const result_category = document.createElement('p');
+        const result_tags = document.createElement('p');
+
+        result_address.classList.add('result-address');
+        result_category.classList.add('result-category');
+        result_tags.classList.add('result-tags');
+
+        result_content.appendChild(result_address);
+        result_content.appendChild(result_category);
+        result_content.appendChild(result_tags);
+
+        const re_load = document.createElement('button');
+        re_load.classList.add('re-load');
+
+        course.appendChild(result_title);
+        course.appendChild(result_href);
+        course.appendChild(result_content);
+        course.appendChild(re_load);
+
+        const del = document.createElement('img');
+        del.classList.add('delete');
+
+        course_result.appendChild(result_img);
+        course_result.appendChild(course);
+        course_result.appendChild(del);
+
+        return course_result;
+    }
+
+    let url = '';
+
+    let location = input();
+    function input() {
+        let location = $('#input').value;
+        return location;
+    }
+
+    $('.category-position').click(function () {
+        const c = makingCourse();
+        COURSE.appendChild(c);
+    });
+
     $('.food').click(function food() {
         i = i + 1;
-        $(document)
-            .find('.course-area')
-            .append(
-                '<div class="course-result">' +
-                    '<img class="resultimg' +
-                    '" src="" />' +
-                    '<div class="course">' +
-                    '<p class="resulttitle' +
-                    '"></p>' +
-                    '<a href"" class="resulthref' +
-                    '">' +
-                    '<p class="resultlink' +
-                    '">링크</p>' +
-                    '</a>' +
-                    '<p class="resultaddress' +
-                    '">1&nbsp;</p>' +
-                    '<p class="resultcategory' +
-                    '">2&nbsp;</p>' +
-                    '<p class="resulttags' +
-                    '">3&nbsp;</p>' +
-                    '</div>' +
-                    '<div class="erase">삭제</div>' +
-                    '<div class="button-area">' +
-                    '<form method="GET" name="result" class="result">' +
-                    '<input type="button" name="" class="rerandom" value="다시뽑기" />' +
-                    '</form>' +
-                    '</div>' +
-                    '</div>',
-            );
-            return 
+        $(document).return;
     });
     $('.cafe').click(function () {
         i = i + 1;
