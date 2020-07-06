678476269b5a3555bd8244987db8d5c2b9b5ac5e
SEOKJUN-KO <tjrwns1021@gmail.com>
2020-07-06 22:36:19 +0900

    bxslider 오류 리얼 수정

--- Project/js/MainHome/home.js
+++ Project/js/MainHome/home.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     $('.add_slide').bxSlider({
         //bXSlider 사용
-        auto: true, // 자동으로 애니메이션 시작
-        speed: 1000, // 애니메이션 속도
-        pause: 1500, // 애니메이션 유지 시간 (1000은 1초)
-        mode: 'fade', // 슬라이드 모드
-        pager: false, // 페이지 표시 사라짐
-        captions: false, // 이미지 위에 텍스트 없음
-        controls: true, // 슬라이더 위 컨트롤 표시
+        auto: true,
+        speed: 500,
+        pause: 4000,
+        mode: 'fade',
+        autoControls: false,
+        pager: false,
     });
 });
